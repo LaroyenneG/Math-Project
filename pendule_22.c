c8f391c6bc9c22bf2766f2609796c9c10266e633
LaroyenneG <laroyenneg@gmail.com>
2017-12-14 19:43:57 +0100

    local

--- pendule_22.c
+++ pendule_22.c
@@ -2,9 +2,13 @@
 // Created by guillaume on 14/12/17.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
 int main(int argc, char** argv) {
 
-
+    setlocale(LC_NUMERIC, "fr_FR.UTF-8");
 
     return 0;
 }
